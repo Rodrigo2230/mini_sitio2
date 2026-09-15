22464d5625edb811903d8059d3c22e6abee72fde
Rodrigo <rodrigoaf2204n@gmail.com>
2026-09-14 22:18:49 -0500

    JS actualizado

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function cambiarMensaje() {
     const mensaje = document.getElementById("mensaje");
+    const extra = document.getElementById("extra");
+
     mensaje.textContent = "¡Gracias por hacer clic en el botón!";
+    extra.textContent = "Este es un texto adicional.";
+}
+
+function mostrarHora() {
+    const ahora = new Date();
+    const hora = ahora.toLocaleTimeString();
+
+    alert("La hora actual es: " + hora);
 }
